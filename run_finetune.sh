a987a63133f91e6c5298c0364af8d9c2d3635de3
Wei-Lun Chen <renchen8394@gmail.com>
2024-09-20 13:59:51 +0800

    adjust pretrain model weight path

--- run_finetune.sh
+++ run_finetune.sh
@@ -3,5 +3,5 @@
 export CUDA_VISIBLE_DEVICES=0
 
 python finetune.py --train --inference \
-    --weight /media/birdsong/disk02/bird-vocal-classification/weights/pretrain/AudioMAE/mr08/lr0002_wd0001_b64_e32/best_e16.pth \
+    --weight ~/Desktop/mr08/lr0002_wd0001_b64_e32/best_e16.pth \
     --batch_size 512 --lr 0.001 --weight_decay 0.0005
