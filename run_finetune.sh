acf2e3336d75435cfcbc00b6dedeb7fe66c7f443
Wei-Lun Chen <renchen8394@gmail.com>
2024-09-20 14:06:46 +0800

    change other params

--- run_finetune.sh
+++ run_finetune.sh
@@ -4,4 +4,4 @@ export CUDA_VISIBLE_DEVICES=0
 
 python finetune.py --train --inference \
     --weight ~/Desktop/mr08/lr0002_wd0001_b64_e32/best_e16.pth \
-    --batch_size 512 --lr 0.001 --weight_decay 0.0005
+    --batch_size 128 --lr 0.001 --weight_decay 0.0005 --epochs 250
