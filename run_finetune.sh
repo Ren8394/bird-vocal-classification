b9857561c68c8250ae171d664d7519566bce8832
ren <renchen8394@gmail.com>
2024-10-25 10:36:21 +0800

    add *.sh

--- run_finetune.sh
+++
@@ -1,7 +0,0 @@
-#!bin/bash
-
-export CUDA_VISIBLE_DEVICES=0
-
-python finetune.py --inference \
-    --weight ~/Desktop/mr08/lr0002_wd0001_b64_e32/best_e16.pth \
-    --batch_size 128 --lr 0.001 --weight_decay 0.0005 --epochs 250
--- run_pretrain.sh
+++
@@ -1,6 +0,0 @@
-#!bin/bash
-
-export CUDA_VISIBLE_DEVICES=0,1
-
-python pretrain.py --train --mask_ratio 0.8 --batch_size 64 --lr 0.0002 --weight_decay 0.0001 \
-    --ckpt /media/birdsong/disk02/bird-vocal-classification/ckpt/pretrain/AudioMAE/mr08/lr0002_wd0001_b64_e32/best.pth.tar
