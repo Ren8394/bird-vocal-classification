4b99bf960bc9ba22d647a6ef85e5d6976949b679
Ren <renchen8394@gmail.com>
2025-01-06 22:27:55 +0800

    feat: add label weight

--- datasets/twbird.py
+++ datasets/twbird.py
@@ -25,12 +25,13 @@ MAX_FREQ = 11000            # Hz
 NORM_MEAN = -6.679596900939941
 NORM_STD = 2.211771011352539
 
+
 class TWBird(IterableDataset):
     def __init__(self, src_file, labeled=False,
-                 window_size=3.0, hop_length=0.5, with_nota=True
+                 window_size=3.0, hop_length=0.5, with_nota=False
                  ):
         super(TWBird, self).__init__()
-        
+
         self.labeled = labeled
         self.window_size = window_size
         self.hop_length = hop_length
@@ -39,22 +40,32 @@ class TWBird(IterableDataset):
         self.file_paths = np.loadtxt(src_file, dtype=str)
         target_df = pd.read_csv(TARGET_PATH, sep=",", header=0)
         self.target = target_df["Label"].values.tolist()
-        self.sub_target = [t.split("-")[0] + "-A" + t.split("-")[1] for t in self.target]
+        self.sub_target = [t.split("-")[0] + "-A" + t.split("-")[1]
+                           for t in self.target]
 
-        self.window_num = int(np.floor((AUDIO_LENGTH - window_size) / hop_length))
+        self.window_num = int(
+            np.floor((AUDIO_LENGTH - window_size) / hop_length))
         self.total_samples = len(self.file_paths) * self.window_num
+
+        self.label_weights = target_df["Median Duration"].to_numpy()
+        self.label_weights = window_size / self.label_weights
+        self.label_weights = np.where(
+            self.label_weights < 1, 1, self.label_weights)
+        if with_nota:
+            self.label_weights = np.append(self.label_weights, 1)
 
     def __len__(self):
         return self.total_samples
 
     def __iter__(self):
         worker_info = torch.utils.data.get_worker_info()
-        
+
         if worker_info is None:  # single-process data loading
             start_idx = 0
             end_idx = len(self.file_paths)
         else:  # in a worker process
-            per_worker = int(np.ceil(len(self.file_paths) / worker_info.num_workers))
+            per_worker = int(
+                np.ceil(len(self.file_paths) / worker_info.num_workers))
             worker_id = worker_info.id
             start_idx = worker_id * per_worker
             end_idx = min(start_idx + per_worker, len(self.file_paths))
@@ -65,14 +76,15 @@ class TWBird(IterableDataset):
 
             for window_idx in range(self.window_num):
                 start_point = int(window_idx * self.hop_length * sr)
-                end_point = int(window_idx * self.hop_length * sr + self.window_size * sr)
-                sliced_waveform = waveform[:, start_point : end_point]
+                end_point = int(window_idx * self.hop_length *
+                                sr + self.window_size * sr)
+                sliced_waveform = waveform[:, start_point: end_point]
 
                 if self.labeled:
                     sliced_waveform = self._roll_mag_aug(sliced_waveform)
-                
+
                 fbank_features = fbank(
-                    sliced_waveform, 
+                    sliced_waveform,
                     htk_compat=True,
                     sample_frequency=sr,
                     window_type="hanning",
@@ -83,7 +95,7 @@ class TWBird(IterableDataset):
 
                 if self.labeled:
                     fbank_features = self._mask_time_freq_aug(fbank_features)
-                
+
                 fbank_features = (fbank_features - NORM_MEAN) / (NORM_STD * 2)
 
                 if self.labeled:
@@ -129,32 +141,41 @@ class TWBird(IterableDataset):
         return masked_spectrogram
 
     def _add_noise_aug(self, spectrogram):
-        noisy_spectrogram = spectrogram + torch.rand(spectrogram.shape[0], spectrogram.shape[1]) * np.random.rand() / 10
-        rolled_noisy_spectrogram = torch.roll(noisy_spectrogram, np.random.randint(-10, 10), 0)
-        
+        noisy_spectrogram = spectrogram + \
+            torch.rand(
+                spectrogram.shape[0], spectrogram.shape[1]) * np.random.rand() / 10
+        rolled_noisy_spectrogram = torch.roll(
+            noisy_spectrogram, np.random.randint(-10, 10), 0)
+
         return rolled_noisy_spectrogram
 
-    def _calculate_overlap(self, label_time:tuple, window_time:tuple) -> float:
-        overlap_start, overlap_end = max(label_time[0], window_time[0]), min(label_time[1], window_time[1])
+    def _calculate_overlap(self, label_time: tuple, window_time: tuple) -> float:
+        overlap_start, overlap_end = max(label_time[0], window_time[0]), min(
+            label_time[1], window_time[1])
         overlap_duration = max(0, overlap_end - overlap_start)
         label_duration = label_time[1] - label_time[0]
-        overlap_percentage = (overlap_duration / label_duration) if label_duration > 0 else 0
+        overlap_percentage = (
+            overlap_duration / label_duration) if label_duration > 0 else 0
         return overlap_percentage
-    
+
     def _get_soft_label(self, filename, start_time, end_time):
         # read labels from the txt file
         labels_df = pd.read_csv(
             f"{LABEL_DIR}/{filename}.txt", sep="\t", header=None,
             names=["start_time", "end_time", "label"], dtype={"start_time": float, "end_time": float, "label": str}
         )
-        labels_df["label"] = labels_df["label"].apply(lambda x: re.sub(r"\d", "", x))   # remove numbers in the label
+        labels_df["label"] = labels_df["label"].apply(
+            lambda x: re.sub(r"\d", "", x))   # remove numbers in the label
 
         # select labels that are overlapped with the current window
-        labels_df = labels_df[(labels_df["start_time"] <= end_time) & (labels_df["end_time"] >= start_time)]
+        labels_df = labels_df[(labels_df["start_time"] <= end_time) & (
+            labels_df["end_time"] >= start_time)]
         if self.with_nota:
-            soft_label = [1/(len(self.target)+1)] * (len(self.target)+1)  # [target, NOTA]  
+            soft_label = [1/(len(self.target)+1)] * \
+                (len(self.target)+1)  # [target, NOTA]
         else:
-            soft_label = [1/(len(self.target))] * (len(self.target))  # [target]
+            soft_label = [1/(len(self.target))] * \
+                (len(self.target))  # [target]
 
         # if no label is found, set the last element NOTA to 1
         if labels_df.empty:
@@ -164,36 +185,39 @@ class TWBird(IterableDataset):
         else:
             for _, row in labels_df.iterrows():
                 label = row["label"]
-                overlap_percentage = self._calculate_overlap(label_time=(row["start_time"], row["end_time"]), window_time=(start_time, end_time))
+                overlap_percentage = self._calculate_overlap(label_time=(
+                    row["start_time"], row["end_time"]), window_time=(start_time, end_time))
                 # label in the target list
                 if label in self.target:
                     soft_label[self.target.index(label)] += overlap_percentage
                 # label in the sub_target list
                 elif label in self.sub_target:
-                    soft_label[self.sub_target.index(label)] += overlap_percentage * 0.6
+                    soft_label[self.sub_target.index(
+                        label)] += overlap_percentage * 0.6
                 # NOTA label
                 else:
                     if self.with_nota:
                         soft_label[-1] += overlap_percentage
 
-        # normalize the soft label, make every element in the list to be in the range of [0, 1]
-        # min-max normalization
-        min_val = min(soft_label)
-        max_val = max(soft_label)
-        soft_label = [(l - min_val) / (max_val - min_val) for l in soft_label]
+        # multiply the label weights
+        soft_label = np.array(soft_label) * self.label_weights
 
         # clip the value to be in the range of [0, 1]
-        # soft_label = [max(0, min(1, l)) for l in soft_label]
+        soft_label = np.where(soft_label < 0, 0, soft_label)
+        soft_label = np.where(soft_label > 1, 1, soft_label)
 
         return soft_label
+
 
 if __name__ == "__main__":
     from torch.utils.data import DataLoader
 
     # dataset = TWBird(src_file="./data/finetune/test.txt", labeled=True)
-    dataset = TWBird(src_file="./data/pretrain/train.txt", window_size=1.0, hop_length=0.5)
+    dataset = TWBird(src_file="./data/pretrain/train.txt",
+                     window_size=1.0, hop_length=0.5)
 
-    dataloader = DataLoader(dataset, batch_size=1, num_workers=4, pin_memory=True)
+    dataloader = DataLoader(dataset, batch_size=1,
+                            num_workers=4, pin_memory=True)
     for i, (feat, l) in enumerate(dataloader):
         print(feat.shape)
         # print(l.shape)
